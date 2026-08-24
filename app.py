0bb4aa03b63461d70c27556027712c780bf290f2
shevdeanushka16 <shevdeanushka16@gmail.com>
2026-08-24 20:12:29 +0530

    Convert script to Flask application

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello from Jenkins")
-print("Completed tutorial 4")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This application was deployed using Jenkins."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
